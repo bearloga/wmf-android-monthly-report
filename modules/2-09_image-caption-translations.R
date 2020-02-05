b2596af0b5a721527073bfebb94338cdbd0cac01
Mikhail Popov <mpopov@wikimedia.org>
2020-02-05 11:45:03 -0500

    Update 2-09_image-caption-translations.R

--- modules/2-09_image-caption-translations.R
+++ modules/2-09_image-caption-translations.R
@@ -2,7 +2,7 @@
 # - image_caption_translations
 
 if (!file.exists(file.path(data_dir, "image_caption_translations.csv"))) {
-  ict_query <- glue("USE wmf;
+  ict_query <- "USE wmf;
 SELECT
   COUNT(1) AS n_cumulative_total,
   SUM(IF(event_timestamp >= '{yyyy_mm}-01', 1, 0)) AS n_new_this_month
@@ -14,8 +14,8 @@ WHERE snapshot = '{yyyy_mm}'
   AND INSTR(event_comment, '#suggestededit-translate') > 0
   -- Revisions that haven't been reverted as of the snapshot:
   AND NOT revision_is_identity_reverted
-  AND NOT revision_is_deleted_by_page_deletion;")
-  image_caption_translations <- wmf::query_hive(ict_query)
+  AND NOT revision_is_deleted_by_page_deletion;"
+  image_caption_translations <- wmf::query_hive(glue(ict_query, .open = "${"))
   readr::write_csv(image_caption_translations, file.path(data_dir, "image_caption_translations.csv"))
 } else {
   message("Loading image caption translation count")
